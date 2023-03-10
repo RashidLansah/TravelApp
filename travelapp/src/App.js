8ed460d347f027df155c6bfd88399e1a55610611
Lansah <rashidlancergh2@gmail.com>
2023-03-10 17:28:18 +0000

    First commit

---
+++ travelapp/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import SearchAppBar from './components/Appbar';
+import ImgMediaCard from './components/Card';
+
+function App() {
+  return (
+    <div>
+    <SearchAppBar/>
+      <div> 
+      <ImgMediaCard />
+      </div>
+   
+    </div>
+  );
+}
+
+export default App;
